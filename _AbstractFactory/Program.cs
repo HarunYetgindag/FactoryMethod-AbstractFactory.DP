ab083bb54403a1b61fec32f1cffa48c0af9bc3cb
HarunYetgindag <37370113+HarunYetgindag@users.noreply.github.com>
2018-10-02 18:45:57 +0300

    Commit

---
+++ _AbstractFactory/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KitaFabrikasi _kita = new Afrika();
+            HayvanlarAlemi _alem = new HayvanlarAlemi(_kita);
+            _alem.BesinZinciriIslet();
+
+            KitaFabrikasi _amerika = new Amerika();
+            _alem = new HayvanlarAlemi(_amerika);
+            _alem.BesinZinciriIslet();
+
+
+            Console.Read();
+
+        }
+    }
+
+
+    /*
+     Etçil ve otçul hayvan
+     Kıta (Afrika, Amerika)
+     Besin zincirininin işletilmesi
+     */
+
+    abstract class KitaFabrikasi
+    {
+        public abstract EtcilHayvan EtcilOlustur();
+        public abstract OtculHayvan OtculOlustur();
+    }
+
+    /* Kıta Oluşturmalar */
+
+    class Amerika : KitaFabrikasi
+    {
+        
+        public override EtcilHayvan EtcilOlustur()
+        {
+            return new Kurt();
+        }
+
+        public override OtculHayvan OtculOlustur()
+        {
+            return new Bizon();
+        }
+    }
+
+    class Afrika : KitaFabrikasi
+    {
+        public override EtcilHayvan EtcilOlustur()
+        {
+            return new Aslan();
+        }
+
+        public override OtculHayvan OtculOlustur()
+        {
+            return new AfrikaAntilobu();
+        }
+    }
+
+    /* ---- */
+
+    abstract class EtcilHayvan
+    {
+        public abstract void Yer(OtculHayvan otcul);
+    }
+
+    abstract class OtculHayvan
+    {
+        //Otçul hayvanda ot yer . abstract metot olarak yazılabilir. etçil hayvan nasıl otçul hayvan yiyorsa aynı şekilde abstract metot yazılabilir.
+    }
+
+    class AfrikaAntilobu : OtculHayvan
+    {
+
+    }
+
+    class Bizon : OtculHayvan
+    {
+
+    }
+
+    class Aslan : EtcilHayvan
+    {
+        public override void Yer(OtculHayvan otcul)
+        {
+            Console.WriteLine(this.GetType().Name + " yer " + otcul.GetType().Name);
+        }
+    }
+
+    class Kurt : EtcilHayvan
+    {
+        public override void Yer(OtculHayvan otcul)
+        {
+            Console.WriteLine(this.GetType().Name + " yer " + otcul.GetType().Name);
+        }
+    }
+
+    class HayvanlarAlemi // Application Class
+    {
+        private OtculHayvan _otcul;
+        private EtcilHayvan _etcil;
+
+        public HayvanlarAlemi(KitaFabrikasi _fabrika)
+        {
+            _otcul = _fabrika.OtculOlustur();
+            _etcil = _fabrika.EtcilOlustur();
+        }
+
+        public void BesinZinciriIslet()
+        {
+            _etcil.Yer(_otcul);
+        }
+    }
+}
---
+++ _FactoryMethod/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dokuman[] _dokumanlar = new Dokuman[2];
+            _dokumanlar[0] = new Ozgecmis();
+            _dokumanlar[1] = new Rapor();
+            foreach (Dokuman d in _dokumanlar)
+            {
+                Console.WriteLine("\n"+d.GetType().Name+"--");
+                foreach (Sayfa s in d.Sayfalar)
+                {
+                    Console.WriteLine(" " + s.GetType().Name + "--");
+                }
+            }
+            Console.Read();
+        }
+    }
+
+    abstract class Sayfa
+    {
+
+    }
+
+    class YetenekSayfasi : Sayfa
+    {
+
+    }
+
+    class EgitimSayfasi : Sayfa
+    {
+
+    }
+
+    class DeneyimSayfasi : Sayfa
+    {
+
+    }
+
+    class GirisSayfasi : Sayfa
+    {
+
+    }
+
+    class SonucSayfasi : Sayfa
+    {
+
+    }
+
+    class OzetSayfasi : Sayfa
+    {
+
+    }
+
+    class BiyografiSayfasi : Sayfa
+    {
+
+    }
+
+    abstract class Dokuman
+    {
+        public Dokuman() // constructor. instance alınınca direk sayfa oluşturulucak
+        {
+            this.SayfaOlustur();
+        }
+
+        private List<Sayfa> _sayfalar = new List<Sayfa>();
+
+        public List<Sayfa> Sayfalar
+        {
+            get
+            {
+                return _sayfalar;
+            }
+        }
+
+        public abstract void SayfaOlustur();
+    }
+
+    class Ozgecmis : Dokuman
+    {
+        
+        public override void SayfaOlustur()
+        {
+            Sayfalar.Add(new GirisSayfasi());
+            Sayfalar.Add(new YetenekSayfasi());
+            Sayfalar.Add(new EgitimSayfasi());
+            Sayfalar.Add(new DeneyimSayfasi());
+        }
+    }
+
+    class Rapor : Dokuman
+    {
+        public override void SayfaOlustur()
+        {
+            Sayfalar.Add(new GirisSayfasi());
+            Sayfalar.Add(new SonucSayfasi());
+            Sayfalar.Add(new OzetSayfasi());
+            Sayfalar.Add(new BiyografiSayfasi());
+        }
+    }
+}
